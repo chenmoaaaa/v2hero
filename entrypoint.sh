cc82d7cb5670f6cb5318a64d5864d4f16d8dbef5
wangfeng769 <wf10518590@gmail.com>
2018-03-08 16:49:29 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+cd /v2raybin
+wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+unzip v2ray.zip 
+cd /v2raybin/v2ray-v$VER-linux-64
+chmod +x v2ray
+chmod +x v2ctl
+
+echo -e -n "$CONFIG_JSON1" > config.json
+echo -e -n "$PORT" >> config.json
+echo -e -n "$CONFIG_JSON2" >> config.json
+echo -e -n "$UUID" >> config.json
+echo -e -n "$CONFIG_JSON3" >> config.json
+
+./v2ray
